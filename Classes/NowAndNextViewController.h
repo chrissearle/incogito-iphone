e1ac39c6512c46634ce9299fda9d2f6bda0a0cf3
Chris Searle <chris@chrissearle.org>
2010-07-14 11:14:31 +0200

    Add now and next framework

---
+++ Classes/NowAndNextViewController.h
@@ -0,0 +1,16 @@
+//
+//  NowAndNextViewController.h
+//  incogito
+//
+//  Created by Chris Searle on 14.07.10.
+//  Copyright 2010 Chris Searle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface NowAndNextViewController : UIViewController <UITableViewDataSource> {
+
+}
+
+@end
