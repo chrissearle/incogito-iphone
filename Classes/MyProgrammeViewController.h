b1a7659a01630151e9c8d267c236d2bcbf10dc36
Chris Searle <chris@chrissearle.org>
2010-07-14 11:17:56 +0200

    Add MyProg basic frame

---
+++ Classes/MyProgrammeViewController.h
@@ -0,0 +1,16 @@
+//
+//  MyProgrammeViewController.h
+//  incogito
+//
+//  Created by Chris Searle on 14.07.10.
+//  Copyright 2010 Chris Searle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MyProgrammeViewController : UIViewController <UITableViewDataSource> {
+
+}
+
+@end
---
+++ Classes/MyProgrammeViewController.m
@@ -0,0 +1,85 @@
+    //
+//  MyProgrammeViewController.m
+//  incogito
+//
+//  Created by Chris Searle on 14.07.10.
+//  Copyright 2010 Chris Searle. All rights reserved.
+//
+
+#import "MyProgrammeViewController.h"
+
+
+@implementation MyProgrammeViewController
+
+/*
+ // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
+        // Custom initialization
+    }
+    return self;
+}
+*/
+
+/*
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView {
+}
+*/
+
+/*
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+*/
+
+/*
+// Override to allow orientations other than the default portrait orientation.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+*/
+
+- (void)didReceiveMemoryWarning {
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+- (void)viewDidUnload {
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+#pragma mark -
+#pragma mark UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+	// Return the number of sessions in the users programme
+	
+	return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"sessionCell"];
+	
+	if (nil == cell) {
+		cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"sessionCell"];
+	}
+	
+	cell.textLabel.text = @"Cell title";
+	cell.detailTextLabel.text = @"Subtitle";
+	
+	return cell;
+}
+
+@end
