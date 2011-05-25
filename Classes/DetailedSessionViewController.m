b35639132163345060f4fad66b9343cf65cbae6e
Chris Searle <chris@chrissearle.org>
2011-05-25 23:22:39 +0200

    Add rotaion for iPhone detail view (xib for iPad needs thinking about)

--- Classes/DetailedSessionViewController.m
+++ Classes/DetailedSessionViewController.m
@@ -225,6 +225,7 @@
 					  "<html>"
 					  "<head>"
 					  "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\"/>"
+                      "<meta name='viewport' content='width=device-width; initial-scale=1.0; maximum-scale=1.0;'>"
 					  "</head>"
 					  "<body>"
 					  "<h1>%@</h1>"
@@ -310,6 +311,10 @@
 	 name: MPMoviePlayerPlaybackDidFinishNotification
 	 object: nil];
 
+}
+
+-(BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    return YES;
 }
 
 @end
