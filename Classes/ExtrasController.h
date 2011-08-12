b79ff7632362730b01eba76f5e1562257e8a846f
Chris Searle <chris@chrissearle.org>
2011-08-12 19:54:43 +0200

    Add HUD when checking for feedback data

--- Classes/ExtrasController.h
+++ Classes/ExtrasController.h
@@ -6,10 +6,12 @@
 
 #import <UIKit/UIKit.h>
 #import <MediaPlayer/MediaPlayer.h>
+#import "MBProgressHUD.h"
+#import "FeedbackAvailability.h"
 
 @class JZSession;
 
-@interface ExtrasController : UIViewController <UITableViewDelegate, UITableViewDataSource> {
+@interface ExtrasController : UIViewController <UITableViewDelegate, UITableViewDataSource, MBProgressHUDDelegate> {
 	JZSession *session;
     
     NSURL *feedbackFormUrl;
@@ -18,6 +20,12 @@
     NSDictionary *sectionCells;
 
     MPMoviePlayerViewController *movie;
+    
+    MBProgressHUD *HUD;
+    
+    IBOutlet UITableView *tv;
+    
+    FeedbackAvailability *feedbackAvailability;
 }
 
 @property (nonatomic, retain) JZSession *session;
@@ -25,6 +33,8 @@
 @property (nonatomic, retain) NSDictionary *sectionCells;
 @property (nonatomic, retain) MPMoviePlayerViewController *movie;
 @property (nonatomic, retain) NSURL *feedbackFormUrl;
+@property (nonatomic, retain) UITableView *tv;
+@property (nonatomic, retain) FeedbackAvailability *feedbackAvailability;
 
 - (void)endVideo:(NSNotification*) aNotification;
 
