0fb1693a201186f7bebc17974bcf7a50ba366543
Chris Searle <chris@chrissearle.org>
2011-08-23 21:16:25 +0200

    Fix up memory allocation from analyze and go to NSDateFormatter

---
+++ Classes/Common/SessionDateConverter.h
@@ -0,0 +1,13 @@
+//
+//  SessionDateConverter.h
+//  incogito
+//
+//  Created by Chris Searle on 23.08.11.
+//  Copyright (c) 2011 Chris Searle. All rights reserved.
+//
+
+
+
+@interface SessionDateConverter : NSObject
+
+@end
---
+++ Classes/Common/SessionDateConverter.m
@@ -0,0 +1,13 @@
+//
+//  SessionDateConverter.m
+//  incogito
+//
+//  Created by Chris Searle on 23.08.11.
+//  Copyright (c) 2011 Chris Searle. All rights reserved.
+//
+
+#import "SessionDateConverter.h"
+
+@implementation SessionDateConverter
+
+@end
